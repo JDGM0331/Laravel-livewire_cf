7700a4648fa73af23e72df9943ca39f742e7c5da
GUEVARA0331 <jdguevara01@misena.edu.co>
2023-02-10 17:57:45 -0500

    Manejo de compoente básicos (inicio)

---
+++ app/Http/Livewire/ShowPosts.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowPosts extends Component
+{
+    public $titulo;
+
+    public function mount($title){
+        $this->titulo = $title;
+    }
+
+    public function render()
+    {
+        return view('livewire.show-posts');
+    }
+}
